2699f587e2248308da6b9c5776f52fd43d0f0c66
amine sadry <amine.sadry@gmail.com>
2015-04-26 17:43:50 +0200

    file system

---
+++ fs-async.js
@@ -0,0 +1,39 @@
+var fs = require('fs');
+
+if( fs.existsSync('temp') ){
+	console.log('Directory exists, removing...');
+	
+	if( fs.existsSync('temp/new.txt')){
+		fs.unlinkSync('temp/new.txt');
+	}
+
+	fs.rmdirSync('temp');
+}
+
+fs.mkdir('temp', function(err){
+
+	fs.exists('temp', function(exists){
+		if(exists) {
+			process.chdir('temp');
+
+			fs.writeFile('test.txt', 'This is some text for test file', function(err){
+
+				fs.rename('test.txt', 'new.txt', function(err){
+
+					fs.stat('new.txt', function(err, stats) {
+
+						console.log('File has size: ' + stats.size + ' Bytes');
+
+						fs.readFile('new.txt', function(err, data) {
+							console.log('File contents: ' + data.toString());
+						});
+
+					});
+
+				});
+
+			})
+		}
+	});
+
+});
